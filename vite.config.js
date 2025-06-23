6057b80f483d30ea15493c0fefbf6789578765ab
thakur-sunnyking <sunnyrajsharmaraj2001@gmail.com>
2025-06-23 13:30:50 +0530

    Final check before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-app/',
+  base: '/todo-app/', // 👈 this is very important for GitHub Pages
 })
