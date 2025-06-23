736ac0697e9637209beea855791bb84af9a61dfe
thakur-sunnyking <sunnyrajsharmaraj2001@gmail.com>
2025-06-23 14:31:13 +0530

    Fix vercel base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-app/', // 👈 this is very important for GitHub Pages
+  base: '/', // <-- this should be "/" for Vercel
 })
